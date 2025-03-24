79ee81e627963fcd4b6dceae0c9c65151cbf6e0b
Colin <colinraminoson@gmail.com>
2025-03-24 11:54:59 +0300

    init back

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connecté"))
+.catch(err => console.log(err));
+
+// Routes
+// test
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur mon API ! 🚀');
+});
+app.listen(PORT, () => console.log(`Serveur démarré sur le port
+${PORT}`));
