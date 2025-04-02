05cba67ff64495f148b0b1d9ebd067bfe49e3a28
KantoAndrianina <kanto.andrianina@yahoo.com>
2025-04-02 18:32:47 +0300

    env

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = 'http://localhost:5001/';
+  private apiUrl = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.monsite.com',
+    apiUrl: 'https://m1p12mean-kanto-francko-frontend.onrender.com/',
   };
   
