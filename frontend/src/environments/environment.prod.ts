1d7e7eced196e277389463d3bc60bbb1f7c3dc5a
KantoAndrianina <kanto.andrianina@yahoo.com>
2025-04-04 11:16:11 +0300

    env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://m1p12mean-kanto-francko-frontend.onrender.com/',
+    apiUrl: 'https://m1p12mean-kanto-francko.onrender.com/',
   };
   
